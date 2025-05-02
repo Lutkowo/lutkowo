f8ec709ebc1ac3cf6fc9e5993c5bd4d037007d97
Threshu <szymon.banach.dev@gmail.com>
2025-05-03 00:54:53 +0200

    Remove compatibility date from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: '2025-05-03',
   modules: [
     '@nuxt/fonts',
     '@nuxt/image',
